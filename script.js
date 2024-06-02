22446893847e06d2d52554a09ecdb9021efc4c0b
Desc <104591921+Desquised@users.noreply.github.com>
2024-06-02 14:53:11 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,103 @@
+const section = document.querySelector("body > div:nth-child(3) .content");
+
+const typewriter = document.createElement("p");
+
+typewriter.style.position = "absolute";
+
+typewriter.style.right = "-245px";
+
+typewriter.style.top = "60%";
+
+typewriter.style.transform = "translateY(-50%)";
+
+typewriter.style.whiteSpace = "pre-wrap";
+
+typewriter.style.overflow = "hidden";
+
+typewriter.style.textAlign = "right";
+
+typewriter.style.direction = "rtl";
+
+section.appendChild(typewriter);
+
+
+
+const text = '"Los ChatBots son la esperanza\n de un mundo conectado en\n época de adversidad."';
+
+
+
+function typeEffect() {
+
+  typewriter.textContent = "";
+
+  let index = 0;
+
+
+
+  function typeCharacter() {
+
+    if (index < text.length) {
+
+      typewriter.textContent += text.charAt(index);
+
+      index++;
+
+      if (typewriter.textContent.endsWith("Mundo")) {
+
+        typewriter.innerHTML += "<br>";
+
+      }
+
+      setTimeout(typeCharacter, 100);
+
+    }
+
+  }
+
+
+
+  typeCharacter();
+
+}
+
+
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+
+      typeEffect();
+
+    }
+
+  });
+
+});
+
+
+
+observer.observe(section);
+
+
+
+const div = document.querySelector("body > div:nth-child(2)");
+
+div.style.background = "url('https://i.ibb.co/dB8Zmwp/wtf.png')";
+
+div.style.backgroundSize = "cover";
+
+
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    var divElement = document.querySelector('body > div:nth-child(1)');
+
+    divElement.style.backgroundImage = "url('https://i.ibb.co/GMH171b/img-s-Daje-PWn-HHUs9-FIP32u-OJb-SA-1.png')"; // Reemplaza 'ruta_de_la_imagen.jpg' con la ruta de tu imagen
+
+    divElement.style.backgroundSize = "cover"; // Ajusta el tamaño de la imagen al contenedor, manteniendo la relación de aspecto
+
+    divElement.style.backgroundRepeat = "no-repeat"; // Evita que la imagen se repita
+
+  });
