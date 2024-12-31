493f28cf243bc1f0e9361247517b4e66278e4c00
Desc <104591921+Desquised@users.noreply.github.com>
2024-12-31 17:29:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ const observer = new IntersectionObserver((entries) => {
 observer.observe(section);
 
 const div = document.querySelector("body > div:nth-child(2)");
-div.style.background = "url('https://i.ibb.co/GWGn4tF/d3848140-5d60-4e9b-abc4-cee2bdc3e924.webp')";
+div.style.background = "url('https://image.lexica.art/full_webp/b104d2da-0017-40f2-9f2e-ce4efbdc64e7')";
 div.style.backgroundSize = "cover";
 
 window.addEventListener('DOMContentLoaded', function() {
